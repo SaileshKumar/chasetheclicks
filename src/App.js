61f00aa60f3ffad5df2c5ed6ffefcda64425f3f3
Sailesh Kumar <saileshkumar@Saileshs-MacBook-Pro.local>
2023-05-23 12:58:27 -0500

    .

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const val = localStorage.getItem("count");
+    if (val) {
+      setCount(parseInt(val));
+    }
+  }, []);
+
+  const onClick = () => {
+    const newVal = count + 1;
+    setCount(newVal);
+    localStorage.setItem("count", newVal);
+  };
+  return (
+    <>
+      <div className="main-counter" id="main-counter">
+        {count}
+      </div>
+      <button id="button" onClick={onClick}>
+        Click Me
+      </button>
+    </>
+  );
+}
+
+export default App;
